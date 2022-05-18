8b6254cd571aaa4a11f76e776e06ff07f1876d91
Chris83 <83odie@gmail.com>
2022-05-17 20:51:59 -0400

    Completed bubble sort project

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,5 +1,22 @@
-def bubble_sort(data)
+def bubble_sort(data, iteration = 0)
+  return data if data.count <= 1
 
+  swapped = false
+  length = data.count - iteration
+
+  for i in 0...length-1
+    if data[i] > data[i+1]
+      d = data[i]
+      data[i] = data[i+1]
+      data[i+1] = d
+      swapped = true
+    end
+  end
+
+  iteration += 1
+  bubble_sort(data, iteration) if swapped
+
+  data
 end
 
-bubble_sort([4, 3, 78, 2, 0, 2])
+p bubble_sort([4, 3, 78, 2, 0, 2])
